f5b98a5b746916397a6fe76d6b284d8e152c872d
gabbaborjaa <gab.borja26@gmail.com>
2021-12-01 13:39:59 -0500

    edit: ui countdown attempt

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,8 @@
-import './js/tripLocation.js';
+// import './js/tripLocation.js';
 import './js/tripCountdown.js';
 
 import './styles/style.scss';
+
+// Event Listeners
+
+document.getElementById("generate").addEventListener("click", calculateDaysToGo);
