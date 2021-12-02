23db0870102febcb12b40b2cb5aebd96568d255b
gabbaborjaa <gab.borja26@gmail.com>
2021-12-01 19:36:46 -0500

    prod: removed const webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const DotEnv = require('dotenv-webpack');
