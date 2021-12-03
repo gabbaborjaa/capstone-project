30aa54a52426e95c53ee30af87fa2ee407332bc1
gabbaborjaa <gab.borja26@gmail.com>
2021-12-03 00:19:36 -0500

    edit

--- src/client/js/weatherData.js
+++ src/client/js/weatherData.js
@@ -1,38 +1,13 @@
+import numeral from "numeral";
+import { format } from "date-fns";
+import parseISO from 'date-fns/parseISO';
+
 const API_URL = ``;
 
 /**
- * Validate the input text.
  * 
- * @param {*} input 
- * @returns true if the input is valid, false otherwise
+ * 
+ * 
+ * 
  */
-function validateInput(input) {
-    // console.log(":: validateInput(input)");
-
-    // console.log(`input: ${input}`);
-
-    // check if the input is not null and not empty
-    const isInputValid = input ? input.length > 0 : false;
-
-    return isInputValid;
-
-    /**
-     * Get data from the Weatherbit service
-     * 
-     * @param
-     * 
-     * @return
-     */
-    async function fetchWeatherData(input) {
-
-        const isInputvalid = validateInput(input);
-
-        if (isInputValid) {
-            let encodedInput = encodeURIComponent(input);
-
-            try {
-
-                const
-            }
-        }
-    }
+function validateInput(input) {}
