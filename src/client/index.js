2db875027efebe5d60711ebee8a9de7ce255c11c
gabbaborjaa <gab.borja26@gmail.com>
2021-12-06 11:25:02 -0500

    jsdoc added

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,10 @@
+// API Function Imports
 import './js/geoLocation.js'
 import './js/weatherData.js'
 import './js/cityPictures.js'
+
+// Countdown Imports
 import './js/tripCountdown.js'
 
-
+// Style Imports
 import './styles/style.scss'
