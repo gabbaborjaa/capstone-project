70490d4aed302c554368d74ef30eb5134ff490ef
gabbaborjaa <gab.borja26@gmail.com>
2021-12-12 20:50:16 -0500

    syntax edit

--- src/client/index.js
+++ src/client/index.js
@@ -1,35 +1,33 @@
+// Style Imports
+import './styles/style.scss'
 // API Function Imports
 import { getGeoNames } from './js/geoLocation.js'
 import { getWeatherData } from './js/weatherData.js'
 import { getPictureData } from './js/cityPictures.js'
-
-// Countdown Imports
 import { tripCountdown } from './js/tripCountdown.js'
 
-// Style Imports
-import './styles/style.scss'
 
-export {
-    tripCountdown,
-    getGeoNames,
-    getWeatherData,
-    getPictureData
-}
-
-// function registerServiceWorker() {
-//     // console.log(":: registerServiceWorker()");
-
-//     if ('serviceWorker' in navigator) {
-//         window.addEventListener('load', () => {
-//             navigator.serviceWorker.register('/service-worker.js')
-//                 .then(registration => {
-//                     console.log('Service Worker registered: ', registration);
-//                 })
-//                 .catch(registrationError => {
-//                     console.log('Service Worker registration failed: ', registrationError);
-//                 });
-//         });
-//     }
+// export {
+//     tripCountdown,
+//     getGeoNames,
+//     getWeatherData,
+//     getPictureData
 // }
 
-// registerServiceWorker();
+function registerServiceWorker() {
+    // console.log(":: registerServiceWorker()");
+
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/service-worker.js')
+                .then(registration => {
+                    console.log('Service Worker registered: ', registration);
+                })
+                .catch(registrationError => {
+                    console.log('Service Worker registration failed: ', registrationError);
+                });
+        });
+    }
+}
+
+registerServiceWorker();
